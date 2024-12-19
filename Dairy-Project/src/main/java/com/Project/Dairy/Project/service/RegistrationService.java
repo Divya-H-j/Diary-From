deb465ec884ea72b3468f67c119b59657533b7bf
Divya-H-j <divyajadhav2221@gmail.com>
2024-12-19 15:33:12 +0530

    first commit

---
+++ Dairy-Project/src/main/java/com/Project/Dairy/Project/service/RegistrationService.java
@@ -0,0 +1,13 @@
+package com.Project.Dairy.Project.service;
+
+import java.util.List;
+
+import com.Project.Dairy.Project.model.Registration;
+
+public interface RegistrationService {
+Registration insertrecord(Registration r);
+List<Registration> getAll();
+void del(int ri);
+Registration updaterecord(int i,Registration r);
+
+}
---
+++ Dairy-Project/src/test/java/com/Project/Dairy/Project/DairyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Dairy.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
